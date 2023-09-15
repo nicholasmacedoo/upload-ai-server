f7e541e83721ecb27c666103aaeeeb03048558cb
Nicholas Macedo <33987691+nicholasmacedoo@users.noreply.github.com>
2023-09-14 23:24:19 -0300

    :sparkles: finish integration resume openai

--- src/routes/generate-ai-completion.ts
+++ src/routes/generate-ai-completion.ts
@@ -5,7 +5,7 @@ import { z } from 'zod'
 import { openai } from "../lib/openai";
 
 export async function generateAICompletionRoute(app: FastifyInstance) {
-    app.post('/ai/complete', async (request) => {
+    app.post('/ai/complete', async (request, reply) => {
         const paramsSchema = z.object({
             videoId: z.string().uuid(),
             template: z.string(),
@@ -14,11 +14,27 @@ export async function generateAICompletionRoute(app: FastifyInstance) {
 
         const { videoId, temperature, template } = paramsSchema.parse(request.params)
 
-        return {
-            videoId, 
-            temperature, 
-            template
+        const video = await prisma.video.findUniqueOrThrow({
+            where: {
+                id: videoId,
+            }
+        })
+
+        if(!video.transcription) {
+            return reply.status(400).send({ error: 'video transcription was not generated yet.'})
         }
+
+        const promptMessage = template.replace('{transcription}', video.transcription)
+
+        const response = await openai.chat.completions.create({
+            model: 'gpt-3.5-turbo-16k',
+            temperature,
+            messages: [
+                { role: 'user', content: promptMessage }
+            ]
+        })
+
+        return response
         
     })
 }
